f4f55a639318fd83ab78adbc1961545d681ca2d8
Sebastian Castillo <tantrum5535@gmail.com>
2022-12-30 18:31:05 -0300

    conectando a la base de datos

---
+++ includes/config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+function conectarDB() : mysqli {
+    $db = mysqli_connect('localhost','root','','bienesraices_crud');
+    if(!$db){
+        echo "error no se pudo conectar";
+        exit;
+    }
+
+    return $db;
+}
